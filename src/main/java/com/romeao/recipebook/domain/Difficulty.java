7a977888bcdb88e149fca749ed237a8c81622ddb
Romeao Jennings <romeao.jennings@gmail.com>
2019-06-20 13:21:07 -0700

    Add remaining entity objects.

---
+++ src/main/java/com/romeao/recipebook/domain/Difficulty.java
@@ -0,0 +1,7 @@
+package com.romeao.recipebook.domain;
+
+public enum Difficulty {
+    EASY,
+    MODERATE,
+    HARD
+}
--- src/main/java/com/romeao/recipebook/domain/Recipe.java
+++ src/main/java/com/romeao/recipebook/domain/Recipe.java
@@ -16,7 +16,16 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // Todo: private Difficulty difficulty
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -89,6 +98,22 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 
     public Set<Ingredient> getIngredients() {
