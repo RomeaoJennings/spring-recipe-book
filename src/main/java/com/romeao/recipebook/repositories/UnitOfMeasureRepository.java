cb538ff5ffd3bf319734aa38c06a11e60553cacd
Romeao Jennings <romeao.jennings@gmail.com>
2019-06-21 19:16:57 -0700

    Add Category and UnitOfMeasure CRUD Repositories.

--- src/main/java/com/romeao/recipebook/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/romeao/recipebook/repositories/UnitOfMeasureRepository.java
@@ -3,4 +3,8 @@ package com.romeao.recipebook.repositories;
 import com.romeao.recipebook.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {}
+import java.util.Optional;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
+}
