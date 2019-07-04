f366eb84799e9bab48073fcb4a4de1273686877d
Romeao Jennings <romeao.jennings@gmail.com>
2019-07-03 20:58:21 -0700

    Add breadcrumbs and refactor

--- src/main/java/com/romeao/recipebook/controllers/IndexController.java
+++ src/main/java/com/romeao/recipebook/controllers/IndexController.java
@@ -1,32 +1,13 @@
 package com.romeao.recipebook.controllers;
 
-import com.romeao.recipebook.dto.RecipeDto;
-import com.romeao.recipebook.dto.converters.RecipeToRecipeDto;
-import com.romeao.recipebook.services.RecipeService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Controller
 public class IndexController {
 
-    private final RecipeService recipeService;
-    private final RecipeToRecipeDto toRecipeDto;
-
-    public IndexController(RecipeService recipeService, RecipeToRecipeDto toRecipeDto) {
-        this.recipeService = recipeService;
-        this.toRecipeDto = toRecipeDto;
-    }
-
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(Model model) {
-        Set<RecipeDto> recipes = new HashSet<>();
-        recipeService.getAllRecipes().forEach(recipe -> recipes.add(toRecipeDto.convert(recipe)));
-        model.addAttribute("recipes", recipes);
-        return "index";
+    public String redirectToRecipes() {
+        return "redirect:/recipes";
     }
-
 }
